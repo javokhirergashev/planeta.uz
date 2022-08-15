f7d4b6da575d981368cc6f7c2fa37cfa87c73c0b
javokhirergashev <iqtidorziyosi@gmail.com>
2022-08-15 11:46:54 +0500

    upload project files

---
+++ widgets/views/counter.php
@@ -0,0 +1,71 @@
+<section class="hero-cr section-padding bg-img bg-fixed" data-overlay-light="9" data-background="frontend-files/img/2.jpg" style="background-image: url(&quot;frontend-files/img/2.jpg&quot;);">
+    <div class="container">
+        <div class="row">
+
+            <div class="intro offset-lg-1 col-lg-10 text-center">
+                <h6 class="sub-title">Who We Are And What Can We do?</h6>
+                <h4>Welcome To Our Agency</h4>
+                <p>We’re full service which <b>means we’ve got you covered</b> on design and content right through to digital. You’ll form a lasting relationship with us, <b>collaboration is central</b> to everything we do. We’ll push you out of your comfort zone from time-to-time, but this is where you’ll shine. Bottom line is <b>we want you to succeed</b> at Faculty.</p>
+            </div>
+
+            <div class="full-width mt-80 mb-80">
+                <div class="row">
+
+                    <div class="col-lg-4">
+                        <div class="item mb-md50">
+                            <span class="icon icon-basic-book-pen"></span>
+                            <h5>Creative Design</h5>
+                            <p>Lorem Ipsum simply dummy text of the printing and type setting industry when an unknown printing simply dummy</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="item bg-color mb-md50">
+                            <span class="icon icon-basic-cards-diamonds"></span>
+                            <h5>Branding</h5>
+                            <p>Lorem Ipsum simply dummy text of the printing and type setting industry when an unknown printing simply dummy</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="item bg-dark">
+                            <span class="icon icon-basic-pencil-ruler"></span>
+                            <h5>Web Development</h5>
+                            <p>Lorem Ipsum simply dummy text of the printing and type setting industry when an unknown printing simply dummy</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="numb text-center mb-md50">
+                    <h3><span class="count">368</span>+</h3>
+                    <h6>Happy Clients</h6>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="numb text-center mb-md50">
+                    <h3><span class="count">964</span>+</h3>
+                    <h6>Projects Compleated</h6>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="numb text-center mb-sm50">
+                    <h3><span class="count">438</span>+</h3>
+                    <h6>Files Downloaded</h6>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="numb text-center">
+                    <h3><span class="count">5946</span>+</h3>
+                    <h6>Lines Of Code</h6>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
