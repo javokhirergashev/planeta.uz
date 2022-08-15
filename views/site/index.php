cb98d93f2852249d02ce04f67e0a131e84f5053a
javokhirergashev <iqtidorziyosi@gmail.com>
2022-08-15 15:15:43 +0500

    upload project files

--- views/site/index.php
+++ views/site/index.php
@@ -6,4 +6,6 @@ $this->title = 'Planeta Travel';
 ?>
 
 <?=\app\widgets\Counter::widget()?>
+<?=\app\widgets\Whyus::widget()?>
+<?=\app\widgets\Destination::widget()?>
 
---
+++ widgets/views/destination.php
@@ -0,0 +1,125 @@
+<section class="works section-padding">
+    <div class="container">
+        <div class="row">
+
+            <div class="section-head text-center col-sm-12">
+                <h6>Our Portfolio</h6>
+                <h4>Latest <span>Creative</span> Work</h4>
+            </div>
+
+            <!-- filter links -->
+            <div class="filtering text-center mb-30 col-sm-12">
+                <div class="filter">
+                    <span data-filter="*" class="active">All</span>
+                    <span data-filter=".brand">Brand</span>
+                    <span data-filter=".web">Design</span>
+                    <span data-filter=".graphic">Graphic</span>
+                </div>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- gallery -->
+            <div class="gallery full-width" style="position: relative; height: 760px;">
+
+                <!-- gallery item -->
+                <div class="col-lg-4 col-md-6 items graphic" style="position: absolute; left: 0px; top: 0px;">
+                    <div class="item-img">
+                        <img src="frontend-files/img/portfolio/1.jpg" alt="image">
+                        <div class="item-img-overlay">
+                            <div class="overlay-info full-width">
+                                <p>Logo | Branding</p>
+                                <h6>Creative Web Design</h6>
+                                <a href="img/portfolio/1.jpg" class="popimg">
+                                    <span class="icon"><i class="fas fa-long-arrow-alt-right"></i></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- gallery item -->
+                <div class="col-lg-4 col-md-6 items web" style="position: absolute; left: 380px; top: 0px;">
+                    <div class="item-img">
+                        <img src="frontend-files/img/portfolio/2.jpg" alt="image">
+                        <div class="item-img-overlay">
+                            <div class="overlay-info full-width">
+                                <p>Logo | Branding</p>
+                                <h6>Creative Web Design</h6>
+                                <a href="img/portfolio/1.jpg" class="popimg">
+                                    <span class="icon"><i class="fas fa-long-arrow-alt-right"></i></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- gallery item -->
+                <div class="col-lg-4 col-md-6 items brand" style="position: absolute; left: 760px; top: 0px;">
+                    <div class="item-img">
+                        <img src="frontend-files/img/portfolio/3.jpg" alt="image">
+                        <div class="item-img-overlay">
+                            <div class="overlay-info full-width">
+                                <p>Logo | Branding</p>
+                                <h6>Creative Web Design</h6>
+                                <a href="img/portfolio/1.jpg" class="popimg">
+                                    <span class="icon"><i class="fas fa-long-arrow-alt-right"></i></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- gallery item -->
+                <div class="col-lg-4 col-md-6 items graphic" style="position: absolute; left: 0px; top: 380px;">
+                    <div class="item-img">
+                        <img src="frontend-files/img/portfolio/4.jpg" alt="image">
+                        <div class="item-img-overlay">
+                            <div class="overlay-info full-width">
+                                <p>Logo | Branding</p>
+                                <h6>Creative Web Design</h6>
+                                <a href="img/portfolio/1.jpg" class="popimg">
+                                    <span class="icon"><i class="fas fa-long-arrow-alt-right"></i></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- gallery item -->
+                <div class="col-lg-4 col-md-6 items web" style="position: absolute; left: 380px; top: 380px;">
+                    <div class="item-img">
+                        <img src="frontend-files/img/portfolio/5.jpg" alt="image">
+                        <div class="item-img-overlay">
+                            <div class="overlay-info full-width">
+                                <p>Logo | Branding</p>
+                                <h6>Creative Web Design</h6>
+                                <a href="img/portfolio/1.jpg" class="popimg">
+                                    <span class="icon"><i class="fas fa-long-arrow-alt-right"></i></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- gallery item -->
+                <div class="col-lg-4 col-md-6 items brand" style="position: absolute; left: 760px; top: 380px;">
+                    <div class="item-img">
+                        <img src="frontend-files/img/portfolio/6.jpg" alt="image">
+                        <div class="item-img-overlay">
+                            <div class="overlay-info full-width">
+                                <p>Logo | Branding</p>
+                                <h6>Creative Web Design</h6>
+                                <a href="img/portfolio/1.jpg" class="popimg">
+                                    <span class="icon"><i class="fas fa-long-arrow-alt-right"></i></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</section>
